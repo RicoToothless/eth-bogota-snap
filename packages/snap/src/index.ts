dc1b8d9e31598fcc1f6ae1f39ac9c683403a2e16
Rico Chen <ricochen@Ricos-MacBook-Pro-2.local>
2022-10-08 10:11:21 -0500

    chore: complete tutorial

--- packages/snap/src/index.ts
+++ packages/snap/src/index.ts
@@ -29,9 +29,9 @@ export const onRpcRequest: OnRpcRequestHandler = ({ origin, request }) => {
           {
             prompt: getMessage(origin),
             description:
-              'This custom confirmation is just for display purposes.',
+              'Hahahahah',
             textAreaContent:
-              'But you can edit the snap source code to make it do something, if you want to!',
+              'Okay I made something, right?',
           },
         ],
       });
